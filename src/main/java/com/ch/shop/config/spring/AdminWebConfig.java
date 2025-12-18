562a98d62a557624e919933a501ba72c6af0ca19
devlearncampus <devlearncampus.com>
2025-12-18 14:50:10 +0900

    5교시 수업

--- src/main/java/com/ch/shop/config/spring/AdminWebConfig.java
+++ src/main/java/com/ch/shop/config/spring/AdminWebConfig.java
@@ -40,7 +40,7 @@ import com.ch.shop.model.board.BoardServiceImpl;
 //MVC에서의 DAO 는 @Repository 를 붙임 
 //MVC에서의 DAO 는 @Service 를 붙임 
 //MVC에서의 특정 분류가 딱히 없음에도 자동으로 올리고 싶다면 @Component
-@ComponentScan(basePackages = {"com.ch.shop.controller.admin","com.ch.shop.model"})
+@ComponentScan(basePackages = {"com.ch.shop.controller.admin"})
 public class AdminWebConfig extends WebMvcConfigurerAdapter{
 	 
 	//DispatcherServlet이 하위 컨트롤러로 부터 반환받은 결과 페이지에 대한 정보는 사실 완전한 JSP경로가 아니므로, 
@@ -56,66 +56,6 @@ public class AdminWebConfig extends WebMvcConfigurerAdapter{
 		return rv;
 	}
 	
-	/*
-	  스프링이 MVC 프레임워크 중 컨트롤러 영역만을 지원하는 것이 아니라, 데이터베이스 관련 제어도 지원하므로, 
-	  지금까지 순수하게 사용해왔던 mybatis를 스프링이 지원하는 mybatis로 전환해본다
-	  스프링이 지원하는 데이터 연동 기술을 사용하려면, spring jdbc 라이브러리를 추가해야 한다..
-	  spring jdbc 검색  
-	 */
-	
-	/*----------------------------------------------------
-	 1) 개발자가 사용하고 싶은데 데이터소스를 결정 
-	 	- 톰켓이 지원하는 JNDI를 사용할 예정 
-	----------------------------------------------------*/
-	public DataSource dataSource() throws NamingException{
-		JndiTemplate jndi = new JndiTemplate();
-		return jndi.lookup("java:comp/env/jndi/mysql", DataSource.class);
-	}
-
-	/*----------------------------------------------------
-	 2) 트랜잭션 매니저 등록 
-	 	- 	스프링은 개발자가 사용하는 기술이 JDBC, Mybatis, Hibernate, JPA 이건 상관없이 
-	 		일관된 방법으로 트랜잭션을 처리할 수있는 방법을 제공해주는데, 개발자는 자신이 사용하는 기술에 따라 
-	 		적절할 트랜잭션 매니저를 등록해야 한다 
-	 		예) JDBC 사용 시 - DataSourceTransactionManager를 빈으로 등록해야 함 
-	 		예) Hibernate 사용 시 - HibernateTransactionManager 를 빈으로 등록해야 함 
-	 		예) Mybatis 사용 시 - DataSourceTransactionManager를 빈으로 등록해야 함 
-	 									특히 Mybatis 의 경우 JDBC와 동일한 DataSourceTransactionManager를 사용하는 이유는?
-	 									사실 Mybatis는 내부적으로, JDBC를 사용하기 때문임..	 		  
-	 		그리고 이 모든 트랜잭션 메니저의 최상단 객체가 바로 PlatformTransactionManager 이다
-	----------------------------------------------------*/
-	@Bean
-	public PlatformTransactionManager transactionManager(SqlSessionFactory sqlSessionFactory) {		
-		return new DataSourceTransactionManager(sqlSessionFactory.getConfiguration().getEnvironment().getDataSource());
-	}
-	
-	/*----------------------------------------------------
-	 3) SqlSession을 관리하는 mybatis의 SqlSessionFactory를 빈으로 등록  
-	----------------------------------------------------*/
-	@Bean
-	public SqlSessionFactory sqlSessoinFactory() throws Exception{
-		
-		//순수 mybatis 프레임워크 자체에서 지원하는 객체가 아니라, mybatis-spring 에서 지원하는 객체인 
-		//SqlSessionFactoryBean ( 끝에 Bean) 을 이용하여 설정xml 파일을 로드한다 
-		
-		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-		
-		//패키지에 포함된 파일의 유형이 클래스가 아닌 경우 더이상 패키지로 표현하지 말고, 일반 디렉토리로 취급해야 한다..
-		sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("com/ch/shop/config/mybatis/config.xml"));
-		
-		sqlSessionFactoryBean.setDataSource(dataSource());
-		
-		return sqlSessionFactoryBean.getObject();
-	}
-	
-	/*----------------------------------------------------
-	 4) SqlSessionTemplate 빈 등록   
-	 	mybatis 사용 시 쿼리문 수행을 위해서는 SqlSession 을 이용했으나, mybatis-spring 에서는 SqlSessionTemplate 객체를 사용해야 함 
-	----------------------------------------------------*/
-	@Bean
-	public SqlSessionTemplate sqlSessionTemplate() throws Exception{
-		return new SqlSessionTemplate(sqlSessoinFactory());
-	}
 	
 
 	//스프링프레임웍을 지배하는 개발원리 중 하나인 DI를 구현하려면 개발자는 사용할 객체들을 미리 빈으로 등록해야 한다..
